52c62ba6d47d5deca43d7bbbf1431c3d32a6ed67
heosangbeom <luv_3_u@naver.com>
2024-11-28 21:55:27 +0900

    feat(system): system
    - 헬스체크

--- src/main/kotlin/com/example/jenkinstest/controller/SystemController.kt
+++ src/main/kotlin/com/example/jenkinstest/controller/SystemController.kt
@@ -1,4 +1,11 @@
 package com.example.jenkinstest.controller
 
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
 class SystemController {
+
+    @GetMapping("/health")
+    fun health() = "OK"
 }
