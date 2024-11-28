d83c74f6b4da7f72cec7d639f01826b4b1e35d09
heosangbeom <luv_3_u@naver.com>
2024-11-28 22:44:02 +0900

    feat(build): actuator 추가
    - 헬스체크

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,6 +27,8 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation ("org.springframework.boot:spring-boot-starter-actuator")
+
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     compileOnly("org.projectlombok:lombok")
     annotationProcessor("org.projectlombok:lombok")
